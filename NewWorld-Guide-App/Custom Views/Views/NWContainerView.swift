a2965ebde3fc69c0bb1b0a38b17fb6f93cbb26f0
CanerCagri <canerrcagrii@gmail.com>
2022-09-08 10:46:07 +0300

    Initial commit

---
+++ NewWorld-Guide-App/Custom Views/Views/NWContainerView.swift
@@ -0,0 +1,28 @@
+//
+//  NWContainerView.swift
+//  NewWorld-Guide-App
+//
+//  Created by Caner Çağrı on 8.09.2022.
+//
+
+import UIKit
+
+class NWContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+    }
+}
---
+++ NewWorld-Guide-App/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NewWorld-Guide-App
+//
+//  Created by Caner Çağrı on 8.09.2022.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    let dungeons = UIView()
+    let contaniers: [UIView] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
