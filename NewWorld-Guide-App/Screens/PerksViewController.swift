4966032a91c4b3d8c1832f33bacb16eabe4ce097
CanerCagri <canerrcagrii@gmail.com>
2022-09-24 15:47:28 +0300

    SearchBar added to Perks view controller.

--- NewWorld-Guide-App/Screens/PerksViewController.swift
+++ NewWorld-Guide-App/Screens/PerksViewController.swift
@@ -11,11 +11,25 @@ import UIKit
 class PerksViewController: UIViewController {
     
     let tableView = UITableView()
+    let searchController = UISearchController()
+    var filteredPerks: [PerkModel] = []
+    var isSearchBarEmpty: Bool {
+      return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    var perks = PerkMockData.mockdata
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
+        configureSearchController()
         configureTableView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
     func configureViewController() {
@@ -33,18 +47,49 @@ class PerksViewController: UIViewController {
         tableView.dataSource = self
         tableView.register(PerksTableViewCell.self, forCellReuseIdentifier: PerksTableViewCell.reuseID)
     }
+    
+    func configureSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.placeholder = "Search for a perkname"
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.sizeToFit()
+        definesPresentationContext = true
+        navigationItem.hidesSearchBarWhenScrolling = false
+        navigationItem.searchController = searchController
+    }
+    
 }
 
 extension PerksViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return PerkMockData.mockdata.count
+        return perks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: PerksTableViewCell.reuseID) as! PerksTableViewCell
-        let currentPerk = PerkMockData.mockdata[indexPath.row]
-        cell.set(perk: currentPerk)
+        print(indexPath.row)
+        cell.set(perk: perks[indexPath.row])
         return cell
     }
+}
+
+extension PerksViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else {
+            
+            perks = PerkMockData.mockdata
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            return
+        }
+        perks = perks.filter { $0.name.lowercased().contains(filter.lowercased()) }
+       
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+
+
 }
