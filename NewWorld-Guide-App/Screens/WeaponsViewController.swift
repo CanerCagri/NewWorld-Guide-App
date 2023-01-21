d980b2f209ebe4ad7e52393d23d242a3af6983fb
CanerCagri <canerrcagrii@gmail.com>
2023-01-21 20:25:16 +0300

    Search controller added to Weapons page.

--- NewWorld-Guide-App/Screens/WeaponsViewController.swift
+++ NewWorld-Guide-App/Screens/WeaponsViewController.swift
@@ -10,19 +10,30 @@ import UIKit
 
 class WeaponsViewController: UIViewController {
     
+    enum Section {
+        case main
+    }
+    
     // MARK: Properties
+    
+    var isSearching = false
+    var weapons = WeaponMockData.mockdata
+    var filteredWeapons: [WeaponModel] = []
     
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource <Section, WeaponModel>!
+    let searchController = UISearchController()
+    
     
     // MARK: Lifecycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
+        configureSearchController()
         configureCollectionView()
         configureDataSource()
-        updateData()
+        updateData(weapons: weapons)
     }
     
     // MARK: Functions
@@ -30,6 +41,14 @@ class WeaponsViewController: UIViewController {
     func configureViewController() {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = titles.weapons
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
+    
+    func configureSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.placeholder = "Search for a username"
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
     }
     
     func configureCollectionView() {
@@ -48,11 +67,28 @@ class WeaponsViewController: UIViewController {
         })
     }
     
-    func updateData() {
+    func updateData(weapons: [WeaponModel]) {
         var snapShot = NSDiffableDataSourceSnapshot<Section, WeaponModel>()
         snapShot.appendSections([.main])
-        snapShot.appendItems(WeaponMockData.mockdata)
+        snapShot.appendItems(weapons)
         
         dataSource.apply(snapShot, animatingDifferences: true)
     }
+}
+
+extension WeaponsViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else {
+            filteredWeapons.removeAll()
+            isSearching = false
+            updateData(weapons: weapons)
+            return
+        }
+        
+        isSearching = true
+        filteredWeapons = weapons.filter { $0.name.lowercased().contains(filter.lowercased()) }
+        updateData(weapons: filteredWeapons)
+    }
+    
+    
 }
