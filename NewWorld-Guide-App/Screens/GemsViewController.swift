59a907e5f35e0b260152ff2edee0fcc6b9999f27
CanerCagri <canerrcagrii@gmail.com>
2023-01-21 20:29:00 +0300

    Search controller added to Gems page.

--- NewWorld-Guide-App/Screens/GemsViewController.swift
+++ NewWorld-Guide-App/Screens/GemsViewController.swift
@@ -10,25 +10,43 @@ import UIKit
 
 class GemsViewController: UIViewController {
     
-    // MARK: Propertie
+    enum Section {
+        case main
+    }
+    
+    // MARK: Properties
+    
+    var isSearching = false
+    var gems = GemMockData.mockdata
+    var filteredGems: [GemModel] = []
     
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource <Section, GemModel>!
+    let searchController = UISearchController()
     
     // MARK: Lifecycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
+        configureSearchController()
         configureCollectionView()
         configureDataSource()
-        updateData()
+        updateData(gems: gems)
     }
     
     // MARK: Functions
     func configureViewController() {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = titles.gems
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
+    
+    func configureSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.placeholder = "Search for a username"
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
     }
     
     func configureCollectionView() {
@@ -48,11 +66,26 @@ class GemsViewController: UIViewController {
         })
     }
     
-    func updateData() {
+    func updateData(gems: [GemModel]) {
         var snapShot = NSDiffableDataSourceSnapshot<Section, GemModel>()
         snapShot.appendSections([.main])
-        snapShot.appendItems(GemMockData.mockdata)
+        snapShot.appendItems(gems)
         
         dataSource.apply(snapShot, animatingDifferences: true)
+    }
+}
+
+extension GemsViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else {
+            filteredGems.removeAll()
+            isSearching = false
+            updateData(gems: gems)
+            return
+        }
+        
+        isSearching = true
+        filteredGems = gems.filter { $0.name.lowercased().contains(filter.lowercased()) }
+        updateData(gems: filteredGems)
     }
 }
